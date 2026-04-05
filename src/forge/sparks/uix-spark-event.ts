54a3202cb93113715871bc822abc6c58f94bdac5
dcapslock <dcapes-davis@outlook.com.au>
2026-04-06 09:10:04 +1000

    fix(event-spark): event spark events causing all forged elements with event spark to refresh even if forge_id does not match

--- src/forge/sparks/uix-spark-event.ts
+++ src/forge/sparks/uix-spark-event.ts
@@ -27,7 +27,7 @@ export class UixForgeSparkDomEvents extends UixForgeSparkBase {
         const eventData = forgeEvent?.data || {};
         if (!eventForgeId) return; // Ignore events without forge_id
         if ( (this.uixForgeMyId && eventForgeId != this.uixForgeMyId) && 
-          (this.uixForgeOthersIds && !this.uixForgeOthersIds.includes(eventForgeId))) {
+          (!this.uixForgeOthersIds || !this.uixForgeOthersIds.includes(eventForgeId))) {
           return;
         }
         this._eventData = this.controller.mergeDeep(this._eventData, { [eventForgeId]: eventData });
