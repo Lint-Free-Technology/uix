04166c451d120a1bdee1de9617719759b48c3f6b
dcapslock <dcapes-davis@outlook.com.au>
2026-08-13 13:18:01 +1000

    refactor: Use js-yaml directly to load theme YAML rather than loading ha-yaml-editor via surrogate tools event tab.
    Fixes: #518

--- src/helpers/yaml2json.ts
+++ src/helpers/yaml2json.ts
@@ -1,69 +1,17 @@
-let _yaml2jsonLoadPromise: Promise<void> | null = null;
+import { load, YAML11_SCHEMA } from 'js-yaml'
 
-const _load_yaml2json = async () => {
-  if (customElements.get("tools-event")) return;
-  if (_yaml2jsonLoadPromise) return _yaml2jsonLoadPromise;
-
-  _yaml2jsonLoadPromise = (async () => {
-    try {
-      await customElements.whenDefined("partial-panel-resolver");
-      const ppr: any = document.createElement("partial-panel-resolver");
-
-      ppr.hass = {
-        panels: [
-          {
-            url_path: "tmp",
-            component_name: "config",
-          },
-        ],
-        auth: {
-          data: {
-            hassUrl: location.origin,
-          }
-        }
-      };
-      ppr._updateRoutes();
-
-      await ppr.routerOptions.routes.tmp.load();
-      await customElements.whenDefined("ha-panel-config");
-      const hpc: any = document.createElement("ha-panel-config");
-      // Home Assistant Developer Tools panel renamed to Tools panel in 2026.8.0
-      if (hpc.routerOptions.routes['developer-tools']) {
-        await hpc.routerOptions.routes['developer-tools']?.load();
-        await customElements.whenDefined("developer-tools-router");
-        const dtr: any = document.createElement("developer-tools-router");
-        await dtr.routerOptions.routes.event.load();
-      } else if (hpc.routerOptions.routes['tools']) {
-        await hpc.routerOptions.routes['tools']?.load();
-        await customElements.whenDefined("tools-router");
-        const tr: any = document.createElement("tools-router");
-        await tr.routerOptions.routes.event.load();
-      } else {
-        console.error("UIX: Error loading yaml2json: Could not find developer-tools or tools route");
-      }
-    } catch (err) {
-      console.error("UIX: Error loading yaml2json:", err);
-    }
-  })();
-
-  return _yaml2jsonLoadPromise;
-};
-
-export const yaml2json = async (yaml) => {
-  await _load_yaml2json();
-  const el: any = document.createElement("ha-yaml-editor");
-  if ('hass' in el) {
-    el.hass = {};
-    el.hass.localize = (any) => "Invalid YAML";
-  } else {
-    el._i18n = { localize: (any) => "Invalid YAML" };
-  }
-  el._onChange(new CustomEvent("yaml", { detail: { value: yaml } }));
-  if (!el.isValid) {
-    console.groupCollapsed("UIX: Error loading theme yaml");
-    console.error(yaml);
+export const yaml2json = async (key, yaml) => {
+  try {
+    const parsed = load(yaml, { schema: YAML11_SCHEMA }) as Record<string, any>;
+    return parsed;
+  } catch (error: any) {
+    const errorMessage = `${error.reason}${error.mark ? ` at line ${error.mark.line + 1}, column ${error.mark.column + 1}` : ''}`;
+    console.groupCollapsed(`UIX: Error loading ${key}`);
+    console.log(errorMessage);
+    console.log(
+      yaml.split('\n').map((line: any, i: number) => `${i == error.mark?.line ? '>>' : '  '}${i + 1}: ${line}`).join('\n')
+    );
     console.groupEnd();
     return {};
   }
-  return el.value;
 };
