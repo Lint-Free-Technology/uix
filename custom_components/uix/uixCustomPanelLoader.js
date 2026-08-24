2bf4808e8c333a61a95275f05f208cb5a34a3571
dcapslock <dcapes-davis@outlook.com.au>
2026-08-24 19:50:10 +1000

    Fix await custom loader import

--- custom_components/uix/uixCustomPanelLoader.js
+++ custom_components/uix/uixCustomPanelLoader.js
@@ -1,2 +1,4 @@
-import(window.parent.customPanelJSOrig || window.parent.customPanelJS);
+(async () => {
+await import(window.parent.customPanelJSOrig || window.parent.customPanelJS);
 import("/uix/uixCustomPanel.js?v=8.1.0");
+})();
